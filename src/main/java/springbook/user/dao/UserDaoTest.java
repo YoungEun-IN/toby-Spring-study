bb41b672f3c73cdf174b8c3bd8b7b59c7c5306df
YoungEun-IN <yyoungeunin@gmail.com>
2019-04-05 13:07:31 +0900

    2.4.2 스프링 테스트 적용 : 테스트를 위한 별도의 DI 설정
    테스트용 설정파일 적용(test-applicationContext.xml)

--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -17,65 +17,64 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations="/applicationContext.xml")
+@ContextConfiguration(locations = "/test-applicationContext.xml")
 public class UserDaoTest {
 	@Autowired
 	ApplicationContext context;
-	
-	private UserDao dao; 
-	
+
+	private UserDao dao;
+
 	private User user1;
 	private User user2;
 	private User user3;
-	
+
 	@Before
 	public void setUp() {
 		this.dao = this.context.getBean("userDao", UserDao.class);
-		
-		this.user1 = new User("gyumee", "π⁄º∫√∂", "springno1");
-		this.user2 = new User("leegw700", "¿Ã±Êø¯", "springno2");
-		this.user3 = new User("bumjin", "π⁄π¸¡¯", "springno3");
+
+		this.user1 = new User("gyumee", "Î∞ïÏÑ±Ï≤†", "springno1");
+		this.user2 = new User("leegw700", "Ïù¥Í∏∏Ïõê", "springno2");
+		this.user3 = new User("bumjin", "Î∞ïÎ≤îÏßÑ", "springno3");
 
 	}
-	
-	@Test 
-	public void andAndGet() throws SQLException, ClassNotFoundException {		
+
+	@Test
+	public void andAndGet() throws SQLException, ClassNotFoundException {
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
 
 		dao.add(user1);
 		dao.add(user2);
 		assertThat(dao.getCount(), is(2));
-		
+
 		User userget1 = dao.get(user1.getId());
 		assertThat(userget1.getName(), is(user1.getName()));
 		assertThat(userget1.getPassword(), is(user1.getPassword()));
-		
+
 		User userget2 = dao.get(user2.getId());
 		assertThat(userget2.getName(), is(user2.getName()));
 		assertThat(userget2.getPassword(), is(user2.getPassword()));
 	}
 
-	@Test(expected=EmptyResultDataAccessException.class)
+	@Test(expected = EmptyResultDataAccessException.class)
 	public void getUserFailure() throws SQLException {
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
-		
+
 		dao.get("unknown_id");
 	}
 
-	
 	@Test
 	public void count() throws SQLException, ClassNotFoundException {
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
-				
+
 		dao.add(user1);
 		assertThat(dao.getCount(), is(1));
-		
+
 		dao.add(user2);
 		assertThat(dao.getCount(), is(2));
-		
+
 		dao.add(user3);
 		assertThat(dao.getCount(), is(3));
 	}
