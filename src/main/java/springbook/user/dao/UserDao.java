ac1c6eac0e5139246c0139ccb2530097d13a6aff
YoungEun-IN <yyoungeunin@gmail.com>
2019-04-05 13:16:43 +0900

    3.4 JdbcContext의 분리, 3.5 콜백과 템플릿의 결합

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -13,41 +13,17 @@ import springbook.user.domain.User;
 
 public class UserDao {
 	private DataSource dataSource;
+	private JdbcContext jdbcContext;
 
 	public void setDataSource(DataSource dataSource) {
+		this.jdbcContext = new JdbcContext();
+		this.jdbcContext.setDataSource(dataSource);
+
 		this.dataSource = dataSource;
 	}
 
-	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-
-		try {
-			c = dataSource.getConnection();
-
-			ps = stmt.makePreparedStatement(c);
-
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
-	}
-
 	public void add(final User user) throws SQLException {
-		jdbcContextWithStatementStrategy(new StatementStrategy() {
+		this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
 			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
 				PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
 				ps.setString(1, user.getId());
@@ -85,49 +61,22 @@ public class UserDao {
 	}
 
 	public void deleteAll() throws SQLException {
-		jdbcContextWithStatementStrategy(new StatementStrategy() {
-			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-				return c.prepareStatement("delete from users");
-			}
-		});
+		this.jdbcContext.executeSql("delete from users");
 	}
 
 	public int getCount() throws SQLException {
-		Connection c = null;
+		Connection c = dataSource.getConnection();
 
-		PreparedStatement ps = null;
+		PreparedStatement ps = c.prepareStatement("select count(*) from users");
 
-		ResultSet rs = null;
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		int count = rs.getInt(1);
 
-		try {
-			c = dataSource.getConnection();
+		rs.close();
+		ps.close();
+		c.close();
 
-			ps = c.prepareStatement("select count(*) from users");
-
-			rs = ps.executeQuery();
-			rs.next();
-			return rs.getInt(1);
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
+		return count;
 	}
 }
