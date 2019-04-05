074b9fc1ed976ea1a5f0903a84d9a833e0696408
YoungEun-IN <yyoungeunin@gmail.com>
2019-04-05 13:09:05 +0900

    3.1 다시 보는 초난감 DAO : 예외처리 기능을 갖춘 DAO

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -57,28 +57,70 @@ public class UserDao {
 	}
 
 	public void deleteAll() throws SQLException {
-		Connection c = dataSource.getConnection();
+		Connection c = null;
 
-		PreparedStatement ps = c.prepareStatement("delete from users");
-		ps.executeUpdate();
+		PreparedStatement ps = null;
 
-		ps.close();
-		c.close();
+		try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("delete from users");
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+
+				} catch (SQLException e) {
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+
+				} catch (SQLException e) {
+				}
+			}
+		}
 	}
 
 	public int getCount() throws SQLException {
-		Connection c = dataSource.getConnection();
+		Connection c = null;
 
-		PreparedStatement ps = c.prepareStatement("select count(*) from users");
+		PreparedStatement ps = null;
 
-		ResultSet rs = ps.executeQuery();
-		rs.next();
-		int count = rs.getInt(1);
+		ResultSet rs = null;
 
-		rs.close();
-		ps.close();
-		c.close();
+		try {
+			c = dataSource.getConnection();
 
-		return count;
+			ps = c.prepareStatement("select count(*) from users");
+
+			rs = ps.executeQuery();
+			rs.next();
+			return rs.getInt(1);
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
 	}
 }
