c04bb13723f7a5909cca09776624498bfd3f057b
YoungEun-IN <yyoungeunin@gmail.com>
2019-04-05 13:14:27 +0900

    3.3 JDBC 전략 패턴의 최적화 : 전략과 클라이언트의 동거
    로컬 클래스, 익명 내부 클래스

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -46,17 +46,17 @@ public class UserDao {
 		}
 	}
 
-	public void add(User user) throws ClassNotFoundException, SQLException {
-		Connection c = dataSource.getConnection();
-		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-		ps.setString(1, user.getId());
-		ps.setString(2, user.getName());
-		ps.setString(3, user.getPassword());
+	public void add(final User user) throws SQLException {
+		jdbcContextWithStatementStrategy(new StatementStrategy() {
+			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+				PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+				ps.setString(1, user.getId());
+				ps.setString(2, user.getName());
+				ps.setString(3, user.getPassword());
 
-		ps.executeUpdate();
-
-		ps.close();
-		c.close();
+				return ps;
+			}
+		});
 	}
 
 	public User get(String id) throws SQLException {
@@ -85,8 +85,11 @@ public class UserDao {
 	}
 
 	public void deleteAll() throws SQLException {
-		StatementStrategy st = new DeleteAllStatement();
-		jdbcContextWithStatementStrategy(st);
+		jdbcContextWithStatementStrategy(new StatementStrategy() {
+			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+				return c.prepareStatement("delete from users");
+			}
+		});
 	}
 
 	public int getCount() throws SQLException {
