c709aef28b0500178640d0ab6d51e2cdd7e34f7f
YoungEun-IN <yyoungeunin@gmail.com>
2019-04-08 14:09:06 +0900

    4 예외
    UserDao 인터페이스 사용, SQLExcption을 런타임 예외로 전환, 데이터 엑세스 기술과 구현방법에 독립적인 Dao 만들기 (JdbcTemplate은 체크예외인 SQLException을 런타임 예외인 DataAccessException 계층구조의 예외로 포장)

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -1,50 +1,19 @@
 package springbook.user.dao;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
-
-import javax.sql.DataSource;
-
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 
 import springbook.user.domain.User;
 
-public class UserDao {
-	public void setDataSource(DataSource dataSource) {
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
-	}
+public interface UserDao {
 
-	private JdbcTemplate jdbcTemplate;
+	void add(User user);
 
-	private RowMapper<User> userMapper = new RowMapper<User>() {
-		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-			User user = new User();
-			user.setId(rs.getString("id"));
-			user.setName(rs.getString("name"));
-			user.setPassword(rs.getString("password"));
-			return user;
-		}
-	};
+	User get(String id);
 
-	public void add(final User user) {
-		this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
-	}
+	List<User> getAll();
 
-	public User get(String id) {
-		return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[] { id }, this.userMapper);
-	}
+	void deleteAll();
 
-	public void deleteAll() {
-		this.jdbcTemplate.update("delete from users");
-	}
+	int getCount();
 
-	public int getCount() {
-		return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
-	}
-
-	public List<User> getAll() {
-		return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
-	}
 }
